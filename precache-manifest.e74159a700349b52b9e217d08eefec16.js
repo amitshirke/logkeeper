aeedc3ed6b6d47cf74b0e3ea49bef98c30956bc4
amitshirke <shirke.amit50@gmail.com>
2019-10-21 15:25:29 +0530

    Updates

---
+++ precache-manifest.e74159a700349b52b9e217d08eefec16.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74d8cbc67e192b43eea7bf574f721a09",
+    "url": "/logkeeper/index.html"
+  },
+  {
+    "revision": "46448ac50a9e9b8f2ba3",
+    "url": "/logkeeper/static/css/2.ae9e8b99.chunk.css"
+  },
+  {
+    "revision": "216cada7a3c9e148059b",
+    "url": "/logkeeper/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "46448ac50a9e9b8f2ba3",
+    "url": "/logkeeper/static/js/2.fadd1756.chunk.js"
+  },
+  {
+    "revision": "216cada7a3c9e148059b",
+    "url": "/logkeeper/static/js/main.05784f6d.chunk.js"
+  },
+  {
+    "revision": "f672b07c6b9afcdec2d3",
+    "url": "/logkeeper/static/js/runtime~main.aefc9e37.js"
+  }
+]);
